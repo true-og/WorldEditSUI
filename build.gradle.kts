1a87501e7554d8289cfce50a4b1d6a1af890607d
NotAlexNoyle <alexnoyle@icloud.com>
2025-09-25 03:24:33 -0400

    Update shadow

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
     id("java")
     id("java-library")
     id("com.diffplug.spotless") version "7.0.4"
-    id("com.gradleup.shadow") version "8.3.6"
+    id("com.gradleup.shadow") version "8.3.8"
     id("checkstyle")
     eclipse
 }
